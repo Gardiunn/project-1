d7d7fbc82e3b91d5195fb5fcacf49f43471681a0
Benjamin Hauk <haukben8@gmail.com>
2024-11-17 15:23:08 -0500

    almost

--- project-1.js
+++ project-1.js
@@ -5,6 +5,11 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import { siteCard } from "./site-card";
+
+let items = [];
+let title = "";
+let siteURL = "";
 
 /**
  * `project-1`
@@ -31,8 +36,6 @@ export class project1 extends DDDSuper(I18NMixin(LitElement)) {
     return [
       super.styles,
       css`
-        :host {
-        }
         #input {
           width: 50%;
           font-size: 24px;
@@ -43,16 +46,26 @@ export class project1 extends DDDSuper(I18NMixin(LitElement)) {
           padding: var(--ddd-spacing-4);
           margin-left: var(--ddd-spacing-2);
         }
-        #inputs {
+        #controls {
           display: flex;
           justify-content: center;
           margin: var(--ddd-spacing-4);
         }
 
         #results {
-          border: var(--ddd-border-md);
-          height: 500px;
-          width: 75%;
+          display: flex;
+          flex-wrap: wrap;
+        }
+        #title {
+          text-align: center;
+        }
+        site-card {
+          width: 20%;
+          min-width: 300px;
+          margin: var(--ddd-spacing-10);
+          border: 1px solid white;
+          position: relative;
+          padding-bottom: 232px;
         }
       `,
     ];
@@ -61,13 +74,72 @@ export class project1 extends DDDSuper(I18NMixin(LitElement)) {
   // Lit render the HTML
   render() {
     return html`
-      <div id="inputs">
+      <div id="controls">
         <input id="input" placeholder="Site address here" />
-        <button id="analyze">Analyze</button>
+        <button id="analyze" @click=${this.analyzeSite}>Analyze</button>
       </div>
+      <div id="title"></div>
 
       <div id="results"></div>
     `;
+  }
+
+  dateToString(timestamp) {
+    const date = new Date(timestamp * 1000);
+    return date.toUTCString();
+  }
+
+  checkSite() {
+    // Get the input value
+    const input = this.shadowRoot.getElementById("input");
+    let site = input.value;
+    // If the user didn't include site.json, add it
+    if (!input.value.includes("site.json")) {
+      site = input.value + "/site.json";
+    }
+    siteURL = site.replace("/site.json", "");
+    return site;
+  }
+
+  async analyzeSite() {
+    // Get the JSON data
+    const site = this.checkSite();
+    const response = await fetch(site);
+    const data = await response.json();
+
+    // Set the title and items
+    title = data.title;
+    items = data.items;
+
+    // get container
+    const container = this.shadowRoot.getElementById("results");
+    // clear container
+    container.innerHTML = "";
+    // get title element
+    const titleElement = this.shadowRoot.getElementById("title");
+    //set title
+    titleElement.innerHTML = data.title;
+
+    for (let i = 0; i < items.length; i++) {
+      const card = document.createElement("site-card");
+
+      card.name = items[i].title;
+      card.description = items[i].description;
+      card.createdTime = this.dateToString(items[i].metadata.created);
+      card.updatedTime = this.dateToString(items[i].metadata.updated);
+
+      if (items[i].metadata.images.length == 0) {
+        card.logo =
+          "https://iam.hax.psu.edu/bmh6200/sites/256/assets/banner.jpg";
+      } else {
+        card.logo = siteURL + items[i].metadata.images[0];
+      }
+      console.log(card.logo);
+
+      container.appendChild(card);
+    }
+
+    console.log(items);
   }
 
   /**
